32dc216c767ca4591089e8276bcdb3d775f5dab5
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-04-16 13:13:39 +0000

    Fix bat file: cd to current dir, not subfolder

--- run_copier.bat
+++ run_copier.bat
@@ -3,7 +3,7 @@ set PROJECT_X_USERNAME=jlab667@gmail.com
 set PROJECT_X_API_KEY=PASTE_YOUR_API_KEY_HERE
 set PROJECT_X_ACCOUNT_NAME=50KTC-V2-261997-32429469
 
-cd /d "%~dp0nq-signal-copier-master"
+cd /d "%~dp0"
 
 python copier_bot.py --ntfy-topic "nqsig-dbcced6e93549dc8" --tg-key "Noisewonderful" --symbol NQ --qty 1
 
